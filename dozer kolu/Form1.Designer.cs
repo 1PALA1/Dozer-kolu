9ed850daace9537683a18774f1d9284bbb63e9eb
İbrahim Emrullah Pala <155005322+1PALA1@users.noreply.github.com>
2024-09-24 01:10:16 +0300

    Add files via upload
    windows form dosyası ile dozer kolunu ve toplama kepçesini ayrı bir şekilde yukarı-aşağı oynatılabilen bir proje

---
+++ dozer kolu/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace dozer_kolu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BASLA = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BASLA
+            // 
+            this.BASLA.Location = new System.Drawing.Point(815, 270);
+            this.BASLA.Name = "BASLA";
+            this.BASLA.Size = new System.Drawing.Size(301, 36);
+            this.BASLA.TabIndex = 0;
+            this.BASLA.Text = "BAŞLA";
+            this.BASLA.UseVisualStyleBackColor = true;
+            this.BASLA.Click += new System.EventHandler(this.BASLA_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(71, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(651, 505);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1255, 628);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BASLA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BASLA;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ dozer kolu/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace dozer_kolu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics cizim;
+        SolidBrush yeşil = new SolidBrush(Color.Green);
+        SolidBrush mavi = new SolidBrush(Color.Blue);
+        SolidBrush kirmizi = new SolidBrush(Color.Red);
+
+        int acikol = 0;
+        int acigövde = 0;
+        int X1 = 200, X2 = 0, X3 = 0, X4 = 0, X5 = 0, X6 = 0;
+        int Y1 = 200, Y2 = 0, Y3 = 0, Y4 = 0, Y5 = 0, Y6 = 0;
+
+        private void BASLA_Click(object sender, EventArgs e)
+        {
+            gövde();
+            kol(acikol);
+            kepce(acigövde);
+            BASLA.Enabled = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                pictureBox1.Refresh();
+                acigövde++;
+                gövde();
+                kol(acikol);
+                kepce(acigövde);
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                pictureBox1.Refresh();
+                acigövde--;
+                gövde();
+                kol(acikol);
+                kepce(acigövde);
+
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                pictureBox1.Refresh();
+                acikol++;
+                gövde();
+                kol(acikol);
+                kepce(acigövde);
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                pictureBox1.Refresh();
+                acikol--;
+                gövde();
+                kol(acikol);
+                kepce(acigövde);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cizim = pictureBox1.CreateGraphics();
+        }
+
+        public double dereceradyan(int aciderece)
+        {
+            double aciradyan = aciderece * 2 * Math.PI / 360;
+            return aciradyan;
+        }
+
+        public void kol(int Aci1)
+        {
+            Point[] kolnokta = new Point[8];
+
+            kolnokta[0].X = X1;
+            kolnokta[0].Y = Y1;
+
+            X2 = Convert.ToInt16(X1 + Math.Cos(dereceradyan(acikol)) * 200);
+            Y2 = Convert.ToInt16(Y1 + Math.Sin(dereceradyan(acikol)) * 200);
+            X3 = Convert.ToInt16(X1 + Math.Cos(dereceradyan(acikol + 90)) * 25);
+            Y3 = Convert.ToInt16(Y1 + Math.Sin(dereceradyan(acikol + 90)) * 25);
+            X4 = Convert.ToInt16(X1 + Math.Cos(dereceradyan(acikol + 7)) * 202);
+            Y4 = Convert.ToInt16(Y1 + Math.Sin(dereceradyan(acikol + 7)) * 202);
+
+
+            kolnokta[0].X = X1;
+            kolnokta[0].Y = Y1;
+            kolnokta[1].X = X2;
+            kolnokta[1].Y = Y2;
+            kolnokta[3].X = X3;
+            kolnokta[3].Y = Y3;
+            kolnokta[2].X = X4;
+            kolnokta[2].Y = Y4;
+            kolnokta[4].X = X1;
+            kolnokta[4].Y = Y1;
+
+            cizim.FillPolygon(mavi, kolnokta);
+        }
+        public void kepce(int Aci2)
+        {
+            Point[] kepcenokta = new Point[4];
+
+            X5 = Convert.ToInt16(X2 + Math.Cos(dereceradyan(acigövde)) * 120);
+            Y5 = Convert.ToInt16(Y2 + Math.Sin(dereceradyan(acigövde)) * 120);
+            X6 = Convert.ToInt16(X2 + Math.Cos(dereceradyan(acigövde + 45)) * 71);
+            Y6 = Convert.ToInt16(Y2 + Math.Sin(dereceradyan(acigövde + 45)) * 71);
+
+
+            kepcenokta[0].X = X2;
+            kepcenokta[0].Y = Y2;
+            kepcenokta[1].X = X5;
+            kepcenokta[1].Y = Y5;
+            kepcenokta[2].X = X6;
+            kepcenokta[2].Y = Y6;
+            kepcenokta[3].X = X2;
+            kepcenokta[3].Y = Y2;
+
+
+            cizim.FillPolygon(kirmizi, kepcenokta);
+        }
+        public void gövde()
+        {
+            Point[] gövdenokta = new Point[5];
+            gövdenokta[0].X = 50;
+            gövdenokta[0].Y = 150;
+            gövdenokta[1].X = 200;
+            gövdenokta[1].Y = 150;
+            gövdenokta[2].X = 200;
+            gövdenokta[2].Y = 300;
+            gövdenokta[3].X = 50;
+            gövdenokta[3].Y = 300;
+            gövdenokta[4].X = 50;
+            gövdenokta[4].Y = 150;
+            cizim.FillPolygon(yeşil, gövdenokta);
+        }
+    }
+}
